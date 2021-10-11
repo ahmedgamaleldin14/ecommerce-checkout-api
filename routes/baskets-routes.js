41e16cabdce26648c973f46d2a84c3e1f6e72520
Ahmed Gamaleldin <ahmedgamal1496@gmail.com>
2021-10-11 16:54:33 +0200

    add body for basket order

--- routes/baskets-routes.js
+++ routes/baskets-routes.js
@@ -8,14 +8,23 @@ const router = express.Router();
 
 router.post(
   '/create/:userId',
-  [check('itemIds').not().isEmpty()],
+  [
+    check('items.*.id').not().isEmpty(),
+    check('items.*.quantity').isFloat({ min: 1 }),
+  ],
   basketValidation(),
   basketControllers.createBasket,
 );
 
 router.post(
   '/:basketid/order',
-  check('stripeTokenId').not().isEmpty(),
+  check('email').isEmail(),
+  check('cardNumber')
+    .isLength({ min: 16, max: 16 })
+    .matches(/\d{16}/),
+  check('expMonth').isLength({ min: 1, max: 2 }),
+  check('expYear').isLength({ min: 4, max: 4 }).matches(/\d{4}/),
+  check('cvc').isLength({ min: 3, max: 3 }).matches(/\d{3}/),
   basketControllers.placeOrder,
 );
 
