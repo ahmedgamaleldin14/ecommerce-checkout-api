d28e3112d68ac159c732bba6124771f9b32be32c
Ahmed Gamaleldin <ahmedgamal1496@gmail.com>
2021-10-11 16:54:44 +0200

    add mocha integration testing

--- test/integration-tests.js
+++ test/integration-tests.js
@@ -1,22 +1,34 @@
 const chai = require('chai');
-const mongoose = require('mongoose');
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
-const server = require('../app');
+const app = require('../app');
+const request = require('supertest')(app);
 const should = chai.should();
 const { expect } = chai;
 
 describe('GET /api/items/:id', () => {
-  it('should return a list of items when called', (done) => {
-    const id = '615f63f7f70f37d0d7bf3053';
-    const expectedItem = { x: 'y' };
+  it('should return an item corresponding to the id when called', (done) => {
+    const id = '61640b17afc5db08eeb3ce04';
+    const expectedItem = {
+      data: {
+        _id: '61640b17afc5db08eeb3ce04',
+        isAvailable: true,
+        name: 'Apple',
+        serialNumber: 'abc-123',
+        price: 10,
+        quantity: 5,
+        __v: 0,
+      },
+    };
     chai
-      .request(server)
+      .request(app)
       .get(`/api/items/${id}`)
       .end((err, res) => {
+        console.log(res);
         res.should.have.status(200);
         expect(res.body).to.deep.equal(expectedItem);
         done();
       });
-  }).timeout(10000);
+  });
 });
